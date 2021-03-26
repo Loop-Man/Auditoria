8d3f4599ca336a559a1643bb8b26fc5ea870a7eb
Loop-Man <60947094+Loop-Man@users.noreply.github.com>
2021-03-26 19:05:53 +0100

    Create hunter-email.sh

---
+++ 2.Footprinting/hunter-email.sh
@@ -0,0 +1,36 @@
+  
+#!/bin/bash
+#author		: Manuel López Torrecillas
+#description: Script para obtener emails del dominio que le pasemos usando la api de hunter.io
+#use: bash hunter-email.sh <domain>
+
+#Colours
+greenColour="\e[0;32m\033[1m"
+endColour="\033[0m\e[0m"
+redColour="\e[0;31m\033[1m"
+blueColour="\e[0;34m\033[1m"
+yellowColour="\e[0;33m\033[1m"
+purpleColour="\e[0;35m\033[1m"
+turquoiseColour="\e[0;36m\033[1m"
+grayColour="\e[0;37m\033[1m"
+
+
+# trap ctrl-c and call ctrl_c()
+trap ctrl_c INT
+
+# Lo primero será crear la función ctrl_c() para crear una salida controlada del script
+function ctrl_c(){
+    # El -e para que no introduzca el echo el new line y tengamos que ponerlo nosotros manualmente (\n)
+    echo -e "\n\n${yellowColour}[*]${endColour}${grayColour} Saliendo de manera controlada${endColour}\n"
+    exit 0
+}
+
+# Fijamos los parámetros de entrada del script a 1.
+let numarg=$(echo $#)
+let totalarg=1
+if [ $numarg -ne $totalarg ];then
+    echo -e "\n\t[*] Usage: bash hunter-email.sh gmail.com\n"
+    exit
+fi
+
+# Declaramos variables del script
